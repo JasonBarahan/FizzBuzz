59edbfc22c18b1b17baa5e7e32100a8eced70aa0
JasonBarahan <122300705+JasonBarahan@users.noreply.github.com>
2023-09-11 11:55:19 -0400

    Added Main.java and modified other files accordingly

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(5);
+        System.out.println(" ");
+        FizzBuzz.doFizzBuzz(99);
+        System.out.println(" ");
+        FizzBuzz.main(null);
+        /*
+        String[] args declares an array of strings as arguments (within variable args)
+
+        I have no args to declare for FizzBuzz.main
+        so I will declare null arguments when calling FizzBuzz.main
+         */
+    }
+}
